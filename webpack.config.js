2200dc568cef56e439614f0371a1ac1fc25113c6
Victor Ferreira <victor.ferreira@ctvoicer.com.br>
2019-04-24 10:58:14 -0300

    refactor: 💡 adapta o projeto para o Webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'ctstyle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'ctstyle.css',
+      chunkFilename: '[id].css'
+    })
+  ]
+}
