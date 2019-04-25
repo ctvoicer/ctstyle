bd69b99b526a1d1728e8197fc25e50bc4c4b9bee
Victor Ferreira <victor.ferreira@ctvoicer.com.br>
2019-04-25 12:25:55 -0300

    feat: 🎸 adiciona o FontAwesome ao projeto

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path')
+const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const devMode = process.env.NODE_ENV.startsWith('dev')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(process.cwd(), 'dist'),
     filename: 'ctstyle.js'
   },
   module: {
@@ -22,10 +26,24 @@ module.exports = {
             options: { sourceMap: true }
           }
         ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
       }
     ]
   },
   plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: !devMode
+    }),
     new MiniCssExtractPlugin({
       filename: 'ctstyle.css',
       chunkFilename: '[id].css'
