53a37bfc9c1d3de76c8e158a2dfd7ff67fb746bc
Victor Ferreira <victor.ferreira@ctvoicer.com.br>
2019-04-25 17:22:22 -0300

    perf: ⚡️ instala o minificador de CSS

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 const devMode = process.env.NODE_ENV.startsWith('dev')
 
@@ -11,34 +12,6 @@ module.exports = {
     path: path.resolve(process.cwd(), 'dist'),
     filename: 'ctstyle.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: { sourceMap: true }
-          },
-          {
-            loader: 'sass-loader',
-            options: { sourceMap: true }
-          }
-        ]
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'fonts/'
-          }
-        }]
-      }
-    ]
-  },
   plugins: [
     new webpack.ProgressPlugin(),
     new CleanWebpackPlugin({
@@ -47,6 +20,30 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'ctstyle.css',
       chunkFilename: '[id].css'
+    }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessorPluginOptions: {
+        preset: ['default', {
+          discardComments: {
+            removeAll: true
+          }
+        }]
+      }
     })
-  ]
+  ],
+  module: {
+    rules: [{
+      test: /\.(sa|sc|c)ss$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+    }, {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/'
+        }
+      }]
+    }]
+  }
 }
